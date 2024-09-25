1976b5152fe40954dbdbaddb986aabb29b7c5008
Faisal <faisal@owimer.co>
2024-09-25 15:39:19 -0400

    Update addBook.sql
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- addBook.sql
+++ addBook.sql
@@ -1,1 +1,1 @@
-INSERT INTO "Book" (title, author, isbn) VALUES ("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565")
+INSERT INTO "Book" (title, author, isbn) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', '978-0743273565')
